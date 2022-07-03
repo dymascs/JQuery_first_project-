700fa7fa1cf35866c756f1792b17a12db7646478
unknown <dymas_cs@hotmail.com>
2022-07-03 16:15:24 -0300

    input field creation and input character and word count

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,5 +1,15 @@
 var frase = $(".frase").text();
 var numPalavra = frase.split("").length;
 var tamanhoFrase = $("#tamanho-frase");
-
 tamanhoFrase.text(numPalavra);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+});
